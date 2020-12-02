79b4352010be6123d07a27d3481231fbbdbf8fe0
jamalmohamad <mohammed.jamalhadi@gmail.com>
2020-12-02 02:06:04 +0100

    remove unused comments

--- testSamples/solutions/distributed/inn.erl
+++ testSamples/solutions/distributed/inn.erl
@@ -31,16 +31,9 @@ inn_adventure() ->
     %GoblinNumbers = (length(Processes) / 2) + 1,
     %io:format("Goblins Numbers: ~p~n", [GoblinNumbers]).
 
-% If he get a request to pass he will decide whether to let the traveler go (handlebed/3 ).
 
-% When a goblin sees a traveler getting on the bed ({on_bed,TravelerId})
-% it will refresh is knowledge of Bed with the TravelId.
-
-% When a goblin sees the traveler leaving ({leaving_bed,TravelerId}) 
-% and the traveler is the one that was in the Bed,  he will now consider the Bed again free.
 goblin() ->
 
-% Each Goblins sees the Bed (initialize them with the atom Bed= free),
     Bed = free,
     receive
         {on_bed, TravelerId} ->
@@ -71,6 +64,7 @@ handlebed(TravelerId, Bed, DiceRoll) when DiceRoll =< 6 ->
         end.
 
 
+% diceroll random number generation
 diceroll(N) when N > 6  -> 0; 
 diceroll(N) when N =< 6 ->
     rand:uniform(N).
